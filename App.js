650f13be2b2797218929e87fba36e6f6cccd3cef
POOJA <121348592+poojaANUP@users.noreply.github.com>
2023-02-23 21:44:48 +0530

    CREATED THE USER SIGNUP FORM

---
+++ App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import MainNavigation from "./components/MainNavigation";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <MainNavigation />
+      <Login />
+    </div>
+  );
+}
+export default App;
